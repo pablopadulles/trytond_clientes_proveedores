a35e17caba51557a9a08eed12d81431dcc444b4f
pablopadulles <pablopadulles@gmail.com>
2021-03-27 18:20:12 -0300

    add random en el telefono

--- party.py
+++ party.py
@@ -16,10 +16,20 @@ from trytond import backend
 from trytond.tools.multivalue import migrate_property
 from trytond.tools import lstrip_wildcard
 from datetime import datetime, timedelta
-
+import random
 
 class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
 
     cliente = fields.Boolean('Cliente')
     proveedor = fields.Boolean('Proveedor')
+
+    def get_random_telefono(self):
+        lista = []
+        for contact in self.contact_mechanisms:
+            if contact.type == 'mobile':
+                lista.append(contact)
+        if lista:
+            res = lista.pop(random.randint(0, len(lista) - 1))
+            return res.value
+        return None
